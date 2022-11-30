59652932f61a46fea300e535c59c7283ab1e07e5
CRaph0705 <raphaelcochrane@hotmail.fr>
2022-11-30 11:09:31 +0100

    challenge 1 js

---
+++ jsChallenge1.js
@@ -0,0 +1,11 @@
+// Déclarer 2 variables: admin et name.
+// Assigner la valeur “John” à name.
+// Copier la valeur de name à admin.
+// Afficher la valeur de admin sur la console.
+'use strict';
+
+let admin = '';
+let name = '';
+name = 'John';
+admin = name;
+console.log(admin);
